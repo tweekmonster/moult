2efae7c296ec0b27ff21eaae2cfcc262f593b922
Tommy Allen <tommy@tweek.us>
2015-05-02 11:40:16 -0400

    Tox tests via setup.py and added flake8 to tox.ini

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 try:
     from pypandoc import convert
@@ -15,6 +16,30 @@ except ImportError:
 moult = __import__('moult')
 
 description = moult.__doc__.strip()
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        import shlex
+
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
 
 setup(
     name='moult',
@@ -46,4 +71,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
+
+    tests_require=['tox'],
+    cmdclass={
+        'test': Tox,
+    }
 )
